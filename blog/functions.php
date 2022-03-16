c58e6f758ceffec289f6d87c280896b78533d58a
FlexCodeur <mehdi.rezgane@campusdulac.com>
2022-03-16 14:21:48 +0100

    Mini projet Blog v1.2.2

--- blog/functions.php
+++ blog/functions.php
@@ -1,26 +1,32 @@
 <?php
 session_start(); // obligatoire pour utiliser les sessions
-// condition pour rediriger vers la homePage si $redirect_to_home existe et n'est pas nul
-if (
-    !isset($_SESSION['id_user'])
-    ||
-    (isset($enable_access)
-        &&
-        isset($_SESSION['id_user'])
-        &&
-        !in_array($_SESSION['role_slug'], $enable_access)
-    )
-) :
-    header('Location: ' . HOME_URL);
-endif;
 
-// Pour éliminer la faille XSS
-function sanitize_html($string)
-{
-    // https://www.php.net/manual/fr/function.htmlspecialchars.php
-    return htmlspecialchars(trim($string));
-}
+// var_dump($_SERVER);
 // on définit une constante de cette façon
 // define('NOM_DE_LA_CONSTANTE_EN_MAJUSCULE', 'VALEUR_DE_LA_CONSTANTE');
 define('HOME_URL', 'http://blog/'); // à chaque migration, cet élément sura surement à changer
 define('PATH_PROJECT', __DIR__);
+
+// condition pour rediriger vers la homePage si $enable_access existe et n'est pas nul
+if($_SERVER['REQUEST_URI'] != '/') : // je verifie que je ne suis pas dans la page home sinon boucle infinie
+	if(
+		!isset($_SESSION['id_user'])  // si je ne suis pas connecté
+		|| // OR
+		(
+			isset($enable_access)
+			&& // ET
+			isset($_SESSION['id_user'])
+			&&
+			// si le rôle n'est pas dans le tableau
+			!in_array($_SESSION['role_slug'], $enable_access)
+		)
+	) :
+		header('Location: ' . HOME_URL);
+	endif;
+endif;
+
+// pour éliminer la faille XSS
+function sanitize_html($string) {
+	// https://www.php.net/manual/fr/function.htmlspecialchars.php
+	return htmlspecialchars(trim($string));
+}
--- blog/views/add_article.php
+++ blog/views/add_article.php
@@ -1,23 +1,28 @@
 <?php
-$enable_access = array('administrator', 'editor');
+// les roles qui ont accès à la page
+// les autres seront redirigés vers la page HOME
+$enable_access = array('administrator', 'editor'); 
+
 require __DIR__ . '/header.php';
-$title = isset($_DET['title']) ? $_GET['title'] : FALSE;
-$title = isset($_DET['content']) ? $_GET['content'] : FALSE;
+$title = isset($_GET['title']) ? $_GET['title'] : FALSE;
+$content = isset($_GET['content']) ? $_GET['content'] : FALSE;
 ?>
 
-<h1>Formulaire d'ajout d'un article</h1>
+<h1 class="title">Formulaire d'ajout d'un article</h1>
+
 <div class="file_form">
-    <form action="<?php echo HOME_URL . 'requests/add_article_post.php'; ?>" method="POST">
-        <div>
-            <label for="title">Titre</label>
-            <input type="text" id="title" name="title">
-        </div>
-        <div>
-            <label for="text">Contenu de l'article</label>
-            <textarea id="text" name="text" rows="5"></textarea>
-        </div>
-        <button type="submit">Valider</button>
-    </form>
+	<form action="<?php echo HOME_URL . 'requests/add_article_post.php'; ?>" method="POST">
+		<div>
+			<label for="title">Titre</label>
+			<input type="text" id="title" name="title" value="<?php if($title) echo $title; ?>">
+		</div>
+		<div>
+			<label for="text">Contenu de l'article</label>
+			<textarea id="text" name="text" rows="10"><?php if($content) echo $content; ?></textarea>
+		</div>
+		<button type="submit">Valider</button>
+	</form>
 </div>
+
 <?php
 require __DIR__ . '/footer.php';
